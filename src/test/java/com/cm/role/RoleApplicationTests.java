08ef88ed0e035bb8780f3cc6d3239042a0b41e34
ConorMolloy <conor.molloy@aexp.com>
2020-06-21 16:40:41 +0100

    Initial commit

---
+++ src/test/java/com/cm/role/RoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cm.role;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
